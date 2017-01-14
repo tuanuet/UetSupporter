ffb6efb294361cb2229a44fda82da953c1ab28e6
Vu Minh Tuan <tuan.uet@gmail.com>
2017-01-14 09:57:31 +0700

    -setting activity

--- app/src/main/java/vnu/uet/tuan/uetsupporter/MainActivity.java
+++ app/src/main/java/vnu/uet/tuan/uetsupporter/MainActivity.java
@@ -86,6 +86,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
